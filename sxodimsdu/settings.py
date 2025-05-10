6c16aca55b0d24474f0185c89499b24875c36f84
S <kgr45637@mail.ru>
2025-05-10 18:37:54 +0500

    added db

--- sxodimsdu/settings.py
+++ sxodimsdu/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'sxodimsdu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'vQGFsGuWyEHifNBctBQJjGJoyDOxzFqm',
+        'HOST': 'yamabiko.proxy.rlwy.net',
+        'PORT': '59715',
     }
 }
 
